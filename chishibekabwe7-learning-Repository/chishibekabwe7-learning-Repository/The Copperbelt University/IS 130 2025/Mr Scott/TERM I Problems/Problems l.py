044546af69494a1c188cb0e5bd32b6689030e340
chishibekabwe7 <chishibekabwe7@gmail.com>
2025-04-11 03:25:18 +0200

    IS 130 Labs Day 1

--- chishibekabwe7-learning-Repository/chishibekabwe7-learning-Repository/The Copperbelt University/IS 130 2025/Mr Scott/TERM I Problems/Problems l.py
+++ chishibekabwe7-learning-Repository/chishibekabwe7-learning-Repository/The Copperbelt University/IS 130 2025/Mr Scott/TERM I Problems/Problems l.py
@@ -1,11 +1,64 @@
 #!/bin/python3
 
+#Problem 13
+"""
+Write a progam that will display the university you will be going to,based on the following conditinos:
+IF the university is CBU and status is "Accepted" then display "The copperbelt University" if the uniersity is UNZA and status is 'Accepted'
+then display The university of Zambia. Else if the university is MU and status is 'Accepted' then display Mulungushi University.
+"""
+institution = str(input("Enter the University you wish to go to:"))
+Status_1 = "Yes"
+Status_2 = "No"
+status = str(input("Do you have an addimissions Letter at that school? Yes/No:"))
+if institution == "CBU" and status == Status_1:
+    print("Your going to the Copperbelt Univeristy")
+elif institution == "UNZA" and status == Status_1:
+    print("Your Going to the University of Zambia")
+elif institution == "MU" and status == Status_1:
+    print("Your going to Mulungushi University")
+else:
+    print("Your not eligile to the institution")
+ 
+    
+
+
+#Problem 12
+"""
+Write a program that asks for the user for their age,and whether they have their
+drivers License. The program should then print out if they are eligible to rent a car or not.
+"""
+age = int(input("Enter Age:"))
+Yes = "Yes"
+No = "No"
+drivers_License = str(input("Do you have a Driver's License? Yes/No:"))
+if age > 21 and drivers_License == Yes:
+    print("You are eligible to rent a car")
+else:
+    print("You are not eligible to rent a car")
+
+
+
+
+#Problem 11
+"""
+Write a Program that will ask for user input and  deterimines if a number is negative,possitive,or zero.
+"""
+Number = int(input("Enter Number"))
+if Number > 0:
+    print("This is a positive Value")
+elif Number == 0:
+    print("This Number is Zero")
+else:
+    print("This is a negative Value")
+
 #Problem 10
-#You are required to write a program that will display the current weather condition and your clothing
-#based on the current temperature. Your program will recieve the current temperature as the input and 
-#check for the following. If the current temperature < 10 then it's freezing and clothing is a Jersey.
-#Else if the current temperature > 30 then it's hot and clothing is a T-shirt,Else if the current 
-# tempetatuer is normal then the clothing is a suit.
+"""
+You are required to write a program that will display the current weather condition and your clothing
+based on the current temperature. Your program will recieve the current temperature as the input and 
+check for the following. If the current temperature < 10 then it's freezing and clothing is a Jersey.
+Else if the current temperature > 30 then it's hot and clothing is a T-shirt,Else if the current 
+temperature is normal then the clothing is a suit.
+"""
 Current_temperature = float(input("Enter the current temperature:"))
 if Current_temperature < 10:
     current_weather = "FREEZING"
@@ -21,11 +74,14 @@ print(f"The current temperature is {current_weather} and the preffered outfit is
 
  
 
+
 #Problem 9
-#Write a program that will display the current wheather conditions based on the current temperature,
-#our program will recieve he current temperature as an input and will check for the following, if the
-#current temperature is less than 10, display "FREEZING", if the current temperature is less than 10,
-#display "HOT" else display normal.
+"""
+Write a program that will display the current wheather conditions based on the current temperature,
+our program will recieve he current temperature as an input and will check for the following, if the
+current temperature is less than 10, display "FREEZING", if the current temperature is less than 10,
+display "HOT" else display normal.
+"""
 current_temperature = int(input("Enter the Current temperature:"))
 if current_temperature < 10:
     print("FREEZING")
@@ -36,10 +92,13 @@ else:
 
 
 
+
 #Problem 8
-#Write a program that will calculate the VAT that will be paid for Tax purchased items, The program
-#should recieve the price of each item and sum them up together. If the sum is greater than 50, VAT
-#will be calculated as the sim multiplied by 0.16 else VAT will be calculated as the sum times 0.05.
+"""
+Write a program that will calculate the VAT that will be paid for Tax purchased items, The program
+should recieve the price of each item and sum them up together. If the sum is greater than 50, VAT
+will be calculated as the sim multiplied by 0.16 else VAT will be calculated as the sum times 0.05.
+"""
 Price_1 = int(input("Enter the first Price:"))
 Price_2 = int(input("Enter the Second Price:")) 
 sum = Price_1 + Price_2
@@ -50,9 +109,11 @@ else:
 print(VAT)
 
 #Problem 7
+"""
 #Zambia recognises 2 types of genders,Male and female,write a program that will ask the user to
 #enter either M for Male and F for Female, the should should check the gender entered and output
 #male if the gender is M else ouput female.
+"""
 Gender = input("Enter Your Gender:")
 Gender2 = "Male"
 if Gender == Gender2:
@@ -62,9 +123,11 @@ else:
 
 
 #Problem 6
-#Write a program that will find out if a vehicle is overspeeding or not,The maximum speed
-#limit in residential areas is 40km/h write a program that will recieve the speed limit of the vehicle,
-#and if the speed of the vehicle is greater than the maximum speed limit, output overspeeding.
+"""
+Write a program that will find out if a vehicle is overspeeding or not,The maximum speed
+limit in residential areas is 40km/h write a program that will recieve the speed limit of the vehicle,
+and if the speed of the vehicle is greater than the maximum speed limit, output overspeeding.
+"""
 Speed = int(input("Enter the speed of a vehicle:"))
 if Speed > 40:
     print("The Vehicle is OverSpeeding!")
