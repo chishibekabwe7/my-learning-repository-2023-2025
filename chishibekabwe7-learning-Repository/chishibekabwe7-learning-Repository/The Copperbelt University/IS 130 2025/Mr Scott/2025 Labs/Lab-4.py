58d10f9046dbd63162322e3f7d8323a8170e8af8
chishibekabwe7 <chishibekabwe7@gmail.com>
2025-04-19 00:50:29 +0200

    IS 130 Labs Dayy 3

---
+++ chishibekabwe7-learning-Repository/chishibekabwe7-learning-Repository/The Copperbelt University/IS 130 2025/Mr Scott/2025 Labs/Lab-4.py
@@ -0,0 +1,113 @@
+#!/bin/python3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #if...elif...else Example
+# """
+# Write a program that asks for a studen't exam score (0-100) and prints their grade
+# """
+# Test_Score = 0
+# Test_Score = int(input("Enter Your Test Score:"))
+# if Test_Score >= 90:
+#     print("Your grade is an A")
+# elif Test_Score >= 80:
+#     print("Your grade is a B")
+# elif Test_Score >= 70:
+#     print("Your grade is a C")
+# elif Test_Score >= 60:
+#     print("Your grade is a D")
+# elif Test_Score <60:
+#     print("Your grade is an F")
+
+
+
+# #if...else statement Example
+# """
+# Write a program that takes a number as input. If the number is even,
+# print 'Even'.Otherwise, print 'Odd'.
+# """
+# Num = 0
+# Num = int(input("Enter Value:"))
+# if Num % 2 == 0:
+#     print(f"{Num} is an even number")
+# else:
+#     print(f"{Num} is an odd number")
+
+# #if statement Example
+# """
+# Write a program that asks the user for their age.
+# If the age is 18 or older, print 'You are eligible to vote'.
+# """
+# age = 0
+# age = int(input("Enter your age:"))
+# if age > 18:
+#     print("You are eligible to vote")
+
+
+
+
+
+
+
+
